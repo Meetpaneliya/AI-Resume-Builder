45f6540017cdf4dec322a4284611eb7cf6da4e91
Meetpaneliya <meetpaneliya032@gmail.com>
2025-01-21 18:45:45 +0530

    commit-5

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +9,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    host: '0.0.0.0', // Bind to 0.0.0.0 to allow external access
+    port: 5173, // You can specify the port you want to use
+  },
+});
